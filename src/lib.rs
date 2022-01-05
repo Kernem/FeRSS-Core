40aa2735e3faff319340d054eed61c2b11084f7b
Stefan Mack <39183620+Areskiko@users.noreply.github.com>
2022-01-05 12:48:42 +0100

    Public api (#20)
    * Hide modules and expose select parts
    
    Hide the entire modules and instead only expose select parts that form the public API

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
-pub mod fetching;
-pub mod processing;
-pub mod structures;
+mod fetching;
+mod processing;
+mod structures;
+
+pub use fetching::functions::get_channels;
+pub use processing::enums;
+pub use structures::channel_collection::ChannelCollection;
